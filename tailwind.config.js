c64e2fd01f9442c9b9e701dfcb5ade864ffdc1e8
dvcuong97 <cuonggdv08@gmail.com>
2023-10-25 18:15:40 +0700

    tailwind purge update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  content: [
-    // Example content paths...
-    "./public/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx,vue}",
-  ],
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
